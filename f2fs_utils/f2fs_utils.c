6f693baf003c1c9ab2d606915315aa16966a8015
Park Ju Hyung <qkrwngud825@gmail.com>
2017-04-02 19:12:22 +0900

    f2fs_utils: Fix for f2fs-tools v1.8.0
     - struct f2fs_configuration must be shared by a pointer.
     - Properly handle sector_size and total_sectors.
     - Trim must be disabled during sparse image creation.
     - Additionally print details if dlopen() fails.
    
    Fixes: 81584836841ef50 ("f2fs_utils: Update for f2fs-tools v1.8.0")
    
    Change-Id: I238b4a787ca71b352322305df48e0e90176f0d7a
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- f2fs_utils/f2fs_utils.c
+++ f2fs_utils/f2fs_utils.c
@@ -42,45 +42,45 @@ struct selabel_handle;
 
 extern void flush_sparse_buffs();
 
-struct f2fs_configuration config;
+extern struct f2fs_configuration *cp;
 struct sparse_file *f2fs_sparse_file;
 extern int dlopenf2fs();
 
 static void reset_f2fs_info() {
 	// Reset all the global data structures used by make_f2fs so it
 	// can be called again.
-	memset(&config, 0, sizeof(config));
+	memset(cp, 0, sizeof(struct f2fs_configuration));
 
-	config.ndevs = 1;
-	config.total_sectors = 0;
-	config.sector_size = 0;
-	config.sectors_per_blk = DEFAULT_SECTORS_PER_BLOCK;
-	config.blks_per_seg = DEFAULT_BLOCKS_PER_SEGMENT;
-	config.wanted_total_sectors = -1;
-	config.zoned_mode = 0;
-	config.zoned_model = 0;
-	config.zone_blocks = 0;
+	cp->ndevs = 1;
+	cp->total_sectors = 0;
+	cp->sector_size = 0;
+	cp->sectors_per_blk = DEFAULT_SECTORS_PER_BLOCK;
+	cp->blks_per_seg = DEFAULT_BLOCKS_PER_SEGMENT;
+	cp->wanted_total_sectors = -1;
+	cp->zoned_mode = 0;
+	cp->zoned_model = 0;
+	cp->zone_blocks = 0;
 
 	for (int i = 0; i < MAX_DEVICES; i++) {
-		memset(&config.devices[i], 0, sizeof(struct device_info));
-		config.devices[i].fd = -1;
-		config.devices[i].sector_size = DEFAULT_SECTOR_SIZE;
-		config.devices[i].end_blkaddr = -1;
-		config.devices[i].zoned_model = F2FS_ZONED_NONE;
+		memset(&cp->devices[i], 0, sizeof(struct device_info));
+		cp->devices[i].fd = -1;
+		cp->devices[i].sector_size = DEFAULT_SECTOR_SIZE;
+		cp->devices[i].end_blkaddr = -1;
+		cp->devices[i].zoned_model = F2FS_ZONED_NONE;
 	}
 
 	/* calculated by overprovision ratio */
-	config.reserved_segments = 0;
-	config.overprovision = 0;
-	config.segs_per_sec = 1;
-	config.secs_per_zone = 1;
-	config.segs_per_zone = 1;
-	config.heap = 1;
-	config.vol_label = "";
-	config.trim = 1;
-	config.ro = 0;
-	config.kd = -1;
-	config.bytes_reserved = 0;
+	cp->reserved_segments = 0;
+	cp->overprovision = 0;
+	cp->segs_per_sec = 1;
+	cp->secs_per_zone = 1;
+	cp->segs_per_zone = 1;
+	cp->heap = 1;
+	cp->vol_label = "";
+	cp->trim = 1;
+	cp->ro = 0;
+	cp->kd = -1;
+	cp->bytes_reserved = 0;
 
 	if (f2fs_sparse_file) {
 		sparse_file_destroy(f2fs_sparse_file);
@@ -97,8 +97,10 @@ int make_f2fs_sparse_fd(int fd, long long len,
 	reset_f2fs_info();
 	f2fs_init_configuration();
 	len &= ~((__u64)(F2FS_BLKSIZE - 1));
-	config.total_sectors = len / config.sector_size;
-	config.start_sector = 0;
+	cp->trim = 0;
+	cp->sector_size = DEFAULT_SECTOR_SIZE;
+	cp->devices[0].total_sectors = len / cp->devices[0].sector_size;
+	cp->start_sector = 0;
 	f2fs_sparse_file = sparse_file_new(F2FS_BLKSIZE, len);
 	f2fs_format_device();
 	sparse_file_write(f2fs_sparse_file, fd, /*gzip*/0, /*sparse*/1, /*crc*/0);
